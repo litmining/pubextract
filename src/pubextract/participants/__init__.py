6261404d26c42ec537d76d10cf236a4fb5df5241
Jerome Dockes <jerome@dockes.org>
2022-11-17 13:53:52 -0500

    add participant demographic information extraction

---
+++ src/pubextract/participants/__init__.py
@@ -0,0 +1,15 @@
+from pubextract.participants._summarization import to_json
+from pubextract.participants._information_extraction import (
+    n_participants_from_labelbuddy_docs,
+    annotate_labelbuddy_docs,
+    extract_from_dataset,
+    Extractor,
+)
+
+__all__ = [
+    "Extractor",
+    "extract_from_dataset",
+    "to_json",
+    "n_participants_from_labelbuddy_docs",
+    "annotate_labelbuddy_docs",
+]
